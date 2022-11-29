e98f60c607b9bc25c635208d882baa572a7105ce
Cuong Phan <codyreactista@gmail.com>
2022-11-29 16:35:29 +0700

    finished

--- app/root.jsx
+++ app/root.jsx
@@ -1,31 +1,91 @@
-const {
+import {
+  Link,
   Links,
   LiveReload,
   Meta,
   Outlet,
   Scripts,
   ScrollRestoration,
-} = require("@remix-run/react");
+  useCatch,
+  useMatches,
+} from "@remix-run/react";
+
+import Error from "./components/util/Error";
+import sharedStyles from "./styles/shared.css";
 
 export const meta = () => ({
   charset: "utf-8",
-  title: "New Remix App",
+  title: "RemixExpenses",
   viewport: "width=device-width,initial-scale=1",
 });
 
-export default function App() {
+function Document({ title, children }) {
+  const matches = useMatches();
+
+  const disableJS = matches.some((match) => match.handle?.disableJS);
+
   return (
     <html lang="en">
       <head>
+        {title && <title>{title}</title>}
         <Meta />
         <Links />
       </head>
       <body>
-        <Outlet />
+        {children}
         <ScrollRestoration />
-        <Scripts />
+        {!disableJS && <Scripts />}
         <LiveReload />
       </body>
     </html>
   );
+}
+
+export default function App() {
+  return (
+    <Document>
+      <Outlet />
+    </Document>
+  );
+}
+
+export function CatchBoundary() {
+  const caughtResponse = useCatch();
+
+  return (
+    <Document title={caughtResponse.statusText}>
+      <main>
+        <Error title={caughtResponse.statusText}>
+          <p>
+            {caughtResponse.data?.message ||
+              "Something went wrong. Please try again later."}
+          </p>
+          <p>
+            Back to <Link to="/">safety</Link>.
+          </p>
+        </Error>
+      </main>
+    </Document>
+  );
+}
+
+export function ErrorBoundary({ error }) {
+  return (
+    <Document title="An error occurred">
+      <main>
+        <Error title="An error occurred">
+          <p>
+            {error.message || "Something went wrong. Please try again later."}
+          </p>
+          <p>
+            Back to <Link to="/">safety</Link>.
+          </p>
+        </Error>
+      </main>
+    </Document>
+  );
+}
+
+export function links() {
+  return [{ rel: "stylesheet", href: sharedStyles }];
 }
---
+++ app/routes/__app/expenses/$id.jsx
@@ -0,0 +1,66 @@
+// /expenses/<some-id> => /expenses/expense-1, /expenses/e-1
+
+import { redirect } from "@remix-run/node";
+import { useNavigate } from "@remix-run/react";
+
+import ExpenseForm from "~/components/expenses/ExpenseForm";
+import Modal from "~/components/util/Modal";
+import { deleteExpense, updateExpense } from "~/data/expenses.server";
+import { validateExpenseInput } from "~/data/validation.server";
+
+export default function UpdateExpensesPage() {
+  const navigate = useNavigate();
+
+  function closeHandler() {
+    // navigate programmatically
+    navigate("..");
+  }
+
+  return (
+    <Modal onClose={closeHandler}>
+      <ExpenseForm />
+    </Modal>
+  );
+}
+
+// export async function loader({ params }) {
+//   const expenseId = params.id;
+//   const expense = await getExpense(expenseId);
+
+//   return expense;
+// }
+
+export async function action({ params, request }) {
+  const expenseId = params.id;
+
+  if (request.method === "PATCH") {
+    const formData = await request.formData();
+    const expenseData = Object.fromEntries(formData);
+
+    try {
+      validateExpenseInput(expenseData);
+    } catch (error) {
+      return error;
+    }
+
+    await updateExpense(expenseId, expenseData);
+
+    return redirect("/expenses");
+  } else if (request.method === "DELETE") {
+    await deleteExpense(expenseId);
+
+    return { deletedId: expenseId };
+    // return redirect("/expenses");
+  }
+}
+
+export function meta({ params, parentsData }) {
+  const expense = parentsData["routes/__app/expenses"].find(
+    (expense) => expense.id === params.id
+  );
+
+  return {
+    title: expense.title,
+    description: "Update expense.",
+  };
+}
